c9272139b514d44b8d7263147e78ed053d663712
kaushal-07 <kingkaushal08@gmail.com>
2021-03-16 16:53:33 +0530

    basic

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 34.66806660728683, lng: 135.4303758435977},
+        zoom: 17,
+        mapId: 'ded739584bffb9dd'
+      });
+}
+//34.66806660728683, 135.4303758435977
